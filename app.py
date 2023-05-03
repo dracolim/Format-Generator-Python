f54c53229699fef208c563ce897a6dec57b0e57b
CherylLim <85498185+dracolim@users.noreply.github.com>
2023-05-03 12:05:26 +0800

    write file completed

--- app.py
+++ app.py
@@ -19,14 +19,44 @@ CORS(app)
 # IMPORT
 @app.route('/import', methods=['POST'])
 def view():
-    file = request.files['file']
-    file.save(file.filename)
+    try: 
+        file = request.files['file']
+        file.save(file.filename)
 
-    filename = file.filename 
+        filename = file.filename 
 
-    with open(filename) as f: 
-        for i , each_line in enumerate(f):
-            
+        variables_list = []
+        with open(filename) as f: 
+            for i , each_line in enumerate(f):
+                edited  = each_line.replace("\\s+" , " ")
+                temp_list = edited.split(" ")
+
+                temp_new_list = []
+
+                for each in temp_list:
+                    if (each != ""):
+                        temp_new_list.append(each.strip())
+
+                for i in range(len(temp_new_list)-1):
+                    item = temp_new_list[i]
+
+                    if "MC" in item:
+                        new_item = temp_new_list[i] + " " + temp_new_list[i + 1]
+                        del temp_new_list[i+1]
+                        temp_new_list[i] = new_item
+
+                # format_line = "%-4s%-8s%-15s%-15s%-16s"
+                # new_line = format_line.format(temp_list[0] , temp_list[1] , temp_list[2] , temp_list[3], temp_list[4])
+               
+                variables = (temp_new_list[0] , temp_new_list[1] , temp_new_list[2] , temp_new_list[3], temp_new_list[4])
+                variables_list.append(variables)
+
+            with open('results.txt', 'w') as writer:
+                for each_var in variables_list:
+                    writer.write('%-4s%-8s%-15s%-15s%-16s \n' % each_var)
+
+    except FileNotFoundError:
+        print("File not Found")
 
     return 'hello'
 
